cb716bdabd8d37ce6236313689557f1a9ff4c8b6
Adrian Jackson <tu001@hpcslurm-controller.us-central1-a.c.cobalt-particle-412908.internal>
2024-11-15 13:49:43 +0000

    Tidying up exercises

--- Exercises/DAOS-MW/clean.sh
+++
@@ -1,1 +0,0 @@
-rm simple_dfs simple_posix simple_mpiio simple_h5 simple_h5.o
--- Exercises/DAOS/run_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -J build
-#SBATCH --nodes=1
-#SBATCH --nvram-options=1LM:1000
-#SBATCH --time=0:10:0
-
-./build.sh
