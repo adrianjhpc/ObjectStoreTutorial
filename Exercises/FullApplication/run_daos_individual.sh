3f2eec651f836682482548a85fb77bf5e22e07af
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2024-11-17 21:22:47 +0000

    Adding in some updates

--- Exercises/FullApplication/run_daos_individual.sh
+++ Exercises/FullApplication/run_daos_individual.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --nodes=2
 #SBATCH --time=00:10:00
-#SBATCH --job-name=daos_example
-#SBATCH -o daos_example.%A.out
-#SBATCH -e daos_example.%A.err
+#SBATCH --job-name=daos_ind_example
+#SBATCH -o daos_individual_example.%A.out
+#SBATCH -e daos_individual_example.%A.err
 #SBATCH --tasks-per-node=32
 #SBATCH --cpus-per-task=1
 
 module load openmpi 
 
-srun ./daos
+srun ./daos_individual
 
